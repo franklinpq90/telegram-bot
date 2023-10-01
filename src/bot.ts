af0ed95e2da1f1d2163e904cb9822be4854f2463
franklinpq90 <146152849+franklinpq90@users.noreply.github.com>
2023-09-30 21:41:02 -0500

    Update bot.ts
    webhook

--- src/bot.ts
+++ src/bot.ts
@@ -3,44 +3,48 @@ dotenv.config();
 import { Bot, webhookCallback } from "grammy";
 import express from "express";
 
-// Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+const GROUP_ID = 352099074; // ID del grupo
 
-// Definición del mapeo de códigos de vecinos
 const neighborsMapping: { [key: string]: string } = {
-    "/001": "El vecino Pepito ha activado la alarma",
-    "/002": "El vecino Ana ha activado la alarma",
-    "/003": "El vecino Luis ha activado la alarma",
-    "/004": "El vecino Marta ha activado la alarma",
-    "/005": "El vecino Pedro ha activado la alarma",
-    "/006": "El vecino Carmen ha activado la alarma",
-    "/007": "El vecino Antonio ha activado la alarma",
-    "/008": "El vecino Teresa ha activado la alarma",
-    "/009": "El vecino José ha activado la alarma",
-    "/010": "El vecino Isabel ha activado la alarma",
+    "001": "El vecino Pepito ha activado la alarma",
+    "002": "El vecino Ana ha activado la alarma",
+    "003": "El vecino Luis ha activado la alarma",
     // ... añade más si es necesario
 };
 
 const registerNeighborCommands = () => {
     for (const [command, message] of Object.entries(neighborsMapping)) {
-        bot.command(command.slice(1), (ctx) => ctx.reply(message));
+        bot.command(command, (ctx) => ctx.reply(message));
     }
 }
 
-registerNeighborCommands();  // Aquí está la llamada que faltaba
+registerNeighborCommands();
 bot.api.setMyCommands([]);
-// Start the server
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+const app = express();
+app.use(express.json());
+
+app.post('/ifttt-webhook', (req, res) => {
+    const userId = req.body.user_id;
+
+    // Verifica si el userId está en nuestro mapeo de vecinos
+    if (neighborsMapping[userId]) {
+        bot.telegram.sendMessage(GROUP_ID, neighborsMapping[userId]);
+    } else {
+        console.log(`Usuario no reconocido: ${userId}`);
+    }
+
+    res.sendStatus(200);
+});
+
+app.use(webhookCallback(bot, "express"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
-  // Use Long Polling for development
+});
+
+if (process.env.NODE_ENV !== "production") {
   bot.start();
 }
