55b72fe80143f3947d3c32929ff72867651613f4
franklinpq90 <146152849+franklinpq90@users.noreply.github.com>
2023-10-01 01:27:01 -0500

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -6,38 +6,17 @@ import fetch from 'node-fetch';
 
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 const USER_ID = 352099074;
-const GROUP_ID = -1001946468061;  // ID del grupo
-const IFTTT_ACTIVATE_URL = `https://maker.ifttt.com/trigger/activate_alarm/with/key/kOlKPd9k75kNySn7c3JSKgnQBDlbXrYd6O6z-O-6CPy`;
-const IFTTT_DEACTIVATE_URL = `https://maker.ifttt.com/trigger/deactivate_alarm/with/key/909qHZ89JlAPXhoRezAYf`;
+const IFTTT_URL = `https://maker.ifttt.com/trigger/activate_alarm/with/key/kOlKPd9k75kNySn7c3JSKgnQBDlbXrYd6O6z-O-6CPy`;
+
+const WAKE_UP_DELAY = 2000; // 2 segundos
 
 const neighborsMapping: { [key: string]: string } = {
-    "001": "🚨🚨🚨 ¡ALERTA DE EMERGENCIA! 🚨🚨🚨\nEl vecino Pepito podría estar en peligro. 🆘❗️\nPor favor, verifica si todo está bien. ¡Actúa con precaución! ⚠️",
-    "002": "🚨🚨🚨 ¡ALERTA DE EMERGENCIA! 🚨🚨🚨\nLa vecina Ana podría estar en peligro. 🆘❗️\nPor favor, verifica si todo está bien. ¡Actúa con precaución! ⚠️",
-    "003": "🚨🚨🚨 ¡ALERTA DE EMERGENCIA! 🚨🚨🚨\nEl vecino Luis podría estar en peligro. 🆘❗️\nPor favor, verifica si todo está bien. ¡Actúa con precaución! ⚠️",
-    "004": "🚨🚨🚨 ¡ALERTA DE EMERGENCIA! 🚨🚨🚨\nEl vecino Rubén podría estar en peligro. 🆘❗️\nPor favor, verifica si todo está bien. ¡Actúa con precaución! ⚠️",
+    "001": "El vecino Pepito ha activado la alarma",
+    "002": "El vecino Ana ha activado la alarma",
+    "003": "El vecino Luis ha activado la alarma",
 };
 
 bot.command('wake_up', (ctx) => { /* simplemente despertar, no hacer nada */ });
-
-bot.command('stop_alarm', async (ctx) => {
-    try {
-        const response = await fetch(IFTTT_DEACTIVATE_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        
-        if (response.ok) {
-            ctx.reply("¡Alarma desactivada! 🔕");
-        } else {
-            ctx.reply("Hubo un problema al intentar desactivar la alarma. Por favor, inténtalo de nuevo.");
-        }
-    } catch (err) {
-        console.error("Error al enviar webhook a IFTTT para desactivar:", err);
-        ctx.reply("Error al intentar desactivar la alarma. Por favor, inténtalo de nuevo.");
-    }
-});
 
 const registerNeighborCommands = () => {
     for (const [command, message] of Object.entries(neighborsMapping)) {
@@ -59,27 +38,22 @@ app.post("/ifttt-webhook", async (req, res) => {
     if (data && data.user_id && data.action === "button_pressed") {
         const messageToSend = neighborsMapping[data.user_id];
         if (messageToSend) {
-            // Enviar el comando "/wake_up" al usuario y al grupo
+            // Enviar el comando "wake_up" para activar al bot
             bot.api.sendMessage(USER_ID, "/wake_up").catch(error => {
-                console.error("Error al enviar el mensaje de despertar al usuario:", error);
-            });
-            bot.api.sendMessage(GROUP_ID, "/wake_up").catch(error => {
-                console.error("Error al enviar el mensaje de despertar al grupo:", error);
+                console.error("Error al despertar el bot:", error);
             });
 
-            // Ahora enviar el mensaje real tanto al usuario como al grupo
+            // Esperar el delay definido
+            await new Promise(resolve => setTimeout(resolve, WAKE_UP_DELAY));
+
+            // Ahora enviar el mensaje real
             bot.api.sendMessage(USER_ID, messageToSend)
                 .catch(error => {
-                    console.error("Error al enviar el mensaje al usuario:", error);
-                });
-            bot.api.sendMessage(GROUP_ID, messageToSend)
-                .catch(error => {
-                    console.error("Error al enviar el mensaje al grupo:", error);
+                    console.error("Error al enviar el mensaje:", error);
                 });
 
-            // Enviar webhook a IFTTT para activar la alarma
             try {
-                const response = await fetch(IFTTT_ACTIVATE_URL, {
+                const response = await fetch(IFTTT_URL, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
