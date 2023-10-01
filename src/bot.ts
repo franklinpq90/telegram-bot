58c4b3a034ba595933d385c73859ddeb244e6bdd
franklinpq90 <146152849+franklinpq90@users.noreply.github.com>
2023-10-01 02:00:59 -0500

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -5,23 +5,19 @@ import express from "express";
 import fetch from 'node-fetch';
 
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
-const USER_ID = 352099074;
-const GROUP_ID = -1001946468061;
+const USER_ID = 352099074; // ID del usuario privado
+const GROUP_ID = -1001946468061; // ID del grupo
 const IFTTT_ACTIVATE_URL = `https://maker.ifttt.com/trigger/activate_alarm/with/key/kOlKPd9k75kNySn7c3JSKgnQBDlbXrYd6O6z-O-6CPy`;
 const IFTTT_DEACTIVATE_URL = `https://maker.ifttt.com/trigger/deactivate_alarm/with/key/909qHZ89JlAPXhoRezAYf`;
 const WAKE_UP_DELAY = 2000;
 
-const neighborsMapping = {
-    "001": "🚨🚨🚨 ¡ALERTA DE EMERGENCIA! 🚨🚨🚨\nEl vecino Pepito podría estar en peligro. 🆘❗️\nPor favor, verifica si todo está bien. ¡Actúa con precaución! ⚠️\nSi todo está en orden, puedes desactivar la alarma.",
-    "002": "🚨🚨🚨 ¡ALERTA DE EMERGENCIA! 🚨🚨🚨\nLa vecina Ana podría estar en peligro. 🆘❗️\nPor favor, verifica si todo está bien. ¡Actúa con precaución! ⚠️\nSi todo está en orden, puedes desactivar la alarma.",
-    "003": "🚨🚨🚨 ¡ALERTA DE EMERGENCIA! 🚨🚨🚨\nEl vecino Luis podría estar en peligro. 🆘❗️\nPor favor, verifica si todo está bien. ¡Actúa con precaución! ⚠️\nSi todo está en orden, puedes desactivar la alarma.",
+const neighborsMapping: { [key: string]: string } = {
+    // ... tus vecinos ...
 };
 
-bot.command('wake_up', (ctx) => {});
+bot.command('wake_up', (ctx) => { /* simplemente despertar, no hacer nada */ });
 
-const inlineKeyboard = new InlineKeyboard().text('Desactivar alarma', 'stop_alarm_callback');
-
-bot.callbackQuery('stop_alarm_callback', async (ctx) => {
+bot.command('stop_alarm', async (ctx) => {
     try {
         const response = await fetch(IFTTT_DEACTIVATE_URL, {
             method: 'POST',
@@ -40,6 +36,8 @@ bot.callbackQuery('stop_alarm_callback', async (ctx) => {
     }
 });
 
+const inlineKeyboard = new InlineKeyboard().text('Desactivar alarma', '/stop_alarm');
+
 const registerNeighborCommands = () => {
     for (const [command, message] of Object.entries(neighborsMapping)) {
         bot.command(command, (ctx) => ctx.reply(message, { reply_markup: inlineKeyboard }));
@@ -53,18 +51,22 @@ const app = express();
 app.use(express.json());
 
 app.post("/ifttt-webhook", async (req, res) => {
+    console.log("Recibida petición desde IFTTT:", req.body);
     const data = req.body;
 
     if (data && data.user_id && data.action === "button_pressed") {
         const messageToSend = neighborsMapping[data.user_id];
         if (messageToSend) {
-            bot.api.sendMessage(USER_ID, "/wake_up").catch(error => console.error("Error al despertar el bot:", error));
-            
+            bot.api.sendMessage(USER_ID, "/wake_up").catch(error => {
+                console.error("Error al despertar el bot:", error);
+            });
             await new Promise(resolve => setTimeout(resolve, WAKE_UP_DELAY));
-            
-            bot.api.sendMessage(USER_ID, messageToSend, { reply_markup: inlineKeyboard }).catch(error => console.error("Error al enviar el mensaje a usuario privado:", error));
-            bot.api.sendMessage(GROUP_ID, messageToSend, { reply_markup: inlineKeyboard }).catch(error => console.error("Error al enviar el mensaje al grupo:", error));
-
+            bot.api.sendMessage(USER_ID, messageToSend, { reply_markup: inlineKeyboard }).catch(error => {
+                console.error("Error al enviar el mensaje a usuario privado:", error);
+            });
+            bot.api.sendMessage(GROUP_ID, messageToSend, { reply_markup: inlineKeyboard }).catch(error => {
+                console.error("Error al enviar el mensaje al grupo:", error);
+            });
             try {
                 const response = await fetch(IFTTT_ACTIVATE_URL, {
                     method: 'POST',
@@ -78,7 +80,6 @@ app.post("/ifttt-webhook", async (req, res) => {
             }
         }
     }
-
     res.status(200).send("OK");
 });
 
