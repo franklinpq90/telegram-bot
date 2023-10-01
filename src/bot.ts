c47bd3fcdadbf64040d681f9798b1f86198e57fc
franklinpq90 <146152849+franklinpq90@users.noreply.github.com>
2023-10-01 00:02:54 -0500

    Update bot.ts
    wake up

--- src/bot.ts
+++ src/bot.ts
@@ -8,11 +8,15 @@ const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 const USER_ID = 352099074;
 const IFTTT_URL = `https://maker.ifttt.com/trigger/activate_alarm/with/key/kOlKPd9k75kNySn7c3JSKgnQBDlbXrYd6O6z-O-6CPy`;
 
+const WAKE_UP_DELAY = 2000; // 2 segundos
+
 const neighborsMapping: { [key: string]: string } = {
     "001": "El vecino Pepito ha activado la alarma",
     "002": "El vecino Ana ha activado la alarma",
     "003": "El vecino Luis ha activado la alarma",
 };
+
+bot.command('wake_up', (ctx) => { /* simplemente despertar, no hacer nada */ });
 
 const registerNeighborCommands = () => {
     for (const [command, message] of Object.entries(neighborsMapping)) {
@@ -34,6 +38,15 @@ app.post("/ifttt-webhook", async (req, res) => {
     if (data && data.user_id && data.action === "button_pressed") {
         const messageToSend = neighborsMapping[data.user_id];
         if (messageToSend) {
+            // Enviar el comando "wake_up" para activar al bot
+            bot.api.sendMessage(USER_ID, "/wake_up").catch(error => {
+                console.error("Error al despertar el bot:", error);
+            });
+
+            // Esperar el delay definido
+            await new Promise(resolve => setTimeout(resolve, WAKE_UP_DELAY));
+
+            // Ahora enviar el mensaje real
             bot.api.sendMessage(USER_ID, messageToSend)
                 .catch(error => {
                     console.error("Error al enviar el mensaje:", error);
